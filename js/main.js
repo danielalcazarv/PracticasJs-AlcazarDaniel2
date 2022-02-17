f8ce42dc86a8657cf8efe84a3ff02d27d8dec59d
Daniel Alcazar <danielalcazar54@gmail.com>
2022-02-16 22:47:56 -0300

    Agregado de array hotels y metodo filter y map

--- js/main.js
+++ js/main.js
@@ -7,6 +7,14 @@ class Producto {
     }
 }
 
+class Producto2 {
+    constructor(estrellas, nombre, precio){
+        this.estrellas = parseInt(estrellas);
+        this.nombre = nombre.toUpperCase();
+        this.precio = parseFloat(precio);
+    }
+}
+
 const parks = [];
 parks.push (new Producto("magic kingdom", "109"));
 parks.push (new Producto("epcot", "109"));
@@ -20,8 +28,13 @@ airline.push (new Producto("AA", "1300"));
 airline.push (new Producto("AR", "1100"));
 airline.push (new Producto("LA", "900"));
 
+const hotels = [];
+hotels.push (new Producto2("3", "value hotels", "68"));
+hotels.push (new Producto2("4", "moderate hotels", "144"));
+hotels.push (new Producto2("5", "deluxe hotels", "350"));
 
-let hotel = 65; //Valor en USD x día
+let cotiHoy = 212;
+let hotel;
 let dias;
 let personas;
 let parquesAcuaticos;
@@ -31,12 +44,36 @@ let totalE = 0;
 let vuelo;
 
 const SUMA = (a,b) => a + b;
+const MULTI = (a,b) => a * b;
+
+const pesificarParks = parks.map ( x => {
+    return {
+        nombre: x.nombre,
+        precio: x.precio * cotiHoy,
+    }
+})
+
+const pesificarAirline = airline.map ( x => {
+    return {
+        nombre: x.nombre,
+        precio: x.precio * cotiHoy,
+    }
+})
+
+const pesificarHotels = hotels.map ( x => {
+    return {
+        estrellas: x.estrellas,
+        nombre: x.nombre,
+        precio: x.precio * cotiHoy,
+    }
+})
 
 function datos (){
     personas = parseInt(prompt("Ingrese la cantidad de personas que viajan"));
     dias = parseInt(prompt ("Ingrese la cantidad de días de su estadía"));
     parquesAcuaticos = prompt ("Desea agregar parques acuáticos?");
     pAtoUpper = parquesAcuaticos.toUpperCase();
+    hotel = parseInt(prompt("Seleccione su hotel 3, 4 o 5 Estrellas"))
 }
 
 function aerolinea(){
@@ -76,43 +113,54 @@ function noAcua (){
     return total;
 }
 
+function estadiaHotel(){
+    hotels.filter(x=>x.estrellas === hotel)
+    let total = 0
+    for (let i = 0; i < hotels.length; i++){
+        total = total + hotels[i].precio;
+    }
+    return total;
+}
+
 do {
     datos();
+    aerolinea();
     if(personas>0 && dias>0 && pAtoUpper === "SI"){
         totalP = personas * parkTotal();
-        totalE = dias * personas * hotel;
+        totalE = dias * personas * estadiaHotel();
     }else if(personas>0 && dias>0){
         noAcua();
         totalP = personas * noAcua();
-        totalE = dias * personas * hotel;
+        totalE = dias * personas * estadiaHotel();
     }
-}while(personas<=0 || dias<=0 || isNaN(personas) || isNaN(dias));
+}while(personas<=0 || dias<=0 || hotel<3 || hotel>5 || isNaN(personas) || isNaN(dias) || isNaN(hotel));
 
 document.write ('<p class="text-normal">Cantidad Total de Personas: '+personas+'</p>');
 document.write ('<p class="text-normal">Estadia total: '+dias+' día/s</p>');
 console.log (totalP);
-document.write ('<p class="text-normal">Valor Final Atracciones Disney 4 días 4 parques temáticos: $' + totalP + ' USD</p>');
+document.write ('<p class="text-normal">Valor Final Atracciones Disney parques temáticos: $' + totalP + ' USD</p>');
 console.log (totalE);
-document.write ('<p class="text-normal">Valor Final por una estadia de ' + dias + ' día/s en un hotel 3 estrellas: $' + totalE + ' USD</p>');
+document.write ('<p class="text-normal">Valor Final por una estadia de ' + dias + ' día/s en un hotel '+hotel+ ' estrellas: $' + totalE + ' USD</p>');
 console.log (pAtoUpper);
+console.log (pesificarParks);
+console.log (pesificarHotels);
+console.log (pesificarAirline);
+console.log (vuelo);
 
-aerolinea();
-let paquete = vuelo;
-console.log (paquete);
-if (paquete ==="AA"){
-    let paqueteAA = SUMA (airline[0].precio,SUMA(totalE,totalP));
+if (vuelo ==="AA"){
     document.write('<p class="text-normal">Viajas por American Airlines. Valor de tu vuelo: $' + airline[0].precio + ' USD</p>');
-    document.write('<p class="text-total">VALOR TOTAL DE TU PAQUETE: $' + paqueteAA +' USD</p>');
-}else if(paquete ==="AR"){
-    let paqueteAR = SUMA (airline[1].precio,SUMA(totalE,totalP));
+    document.write('<p class="text-total">VALOR TOTAL EN DOLARES: $' + SUMA (airline[0].precio,SUMA(totalE,totalP)) +' USD</p>');
+    document.write('<p class="text-total">VALOR TOTAL EN PESOS: $' + MULTI(SUMA (airline[0].precio,SUMA(totalE,totalP)),cotiHoy) +' ARG</p>');
+}else if(vuelo ==="AR"){
     document.write('<p class="text-normal">Viajas por Aerolineas Argentinas. Valor de tu vuelo: $' + airline[1].precio + ' USD</p>');
-    document.write('<p class="text-total">VALOR TOTAL DE TU PAQUETE: $' + paqueteAR +' USD</p>');
-}else if(paquete ==="LA"){
-    let paqueteLA = SUMA (airline[2].precio,SUMA(totalE,totalP));
+    document.write('<p class="text-total">VALOR TOTAL EN DOLARES: $' + SUMA (airline[1].precio,SUMA(totalE,totalP)) +' USD</p>');
+    document.write('<p class="text-total">VALOR TOTAL EN PESOS: $' + MULTI(SUMA (airline[1].precio,SUMA(totalE,totalP)),cotiHoy) +' ARG</p>');
+}else if(vuelo ==="LA"){
     document.write('<p class="text-normal">Viajas por LATAM. Valor de tu vuelo: $' + airline[2].precio + ' USD</p>');
-    document.write('<p class="text-total">VALOR TOTAL DE TU PAQUETE: $' + paqueteLA +' USD</p>');
+    document.write('<p class="text-total">VALOR TOTAL EN DOLARES: $' + SUMA (airline[2].precio,SUMA(totalE,totalP)) +' USD</p>');
+    document.write('<p class="text-total">VALOR TOTAL EN PESOS: $' + MULTI(SUMA (airline[2].precio,SUMA(totalE,totalP)),cotiHoy) +' ARG</p>');
 }else{
-    let paqueteNull = SUMA (totalE,totalP);
     document.write('<p class="text-normal">No seleccionaste ninguna aerolinea</p>');
-    document.write('<p class="text-total"   >VALOR TOTAL DE TU PAQUETE: $' + paqueteNull +' USD</p>');
+    document.write('<p class="text-total">VALOR TOTAL EN DOLARES: $' + SUMA (totalE,totalP) +' USD</p>');
+    document.write('<p class="text-total">VALOR TOTAL EN PESOS: $' + MULTI(SUMA(totalE,totalP),cotiHoy) +' ARG</p>');
 }
